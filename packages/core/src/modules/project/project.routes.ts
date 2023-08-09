e0c71bb9e8035b148209fa601224f731b1d9bf81
RakeshPotnuru <rakeshpotnuru321@gmail.com>
2023-08-09 23:40:12 +0530

    sprint-1.12.3

--- packages/core/src/modules/project/project.routes.ts
+++ packages/core/src/modules/project/project.routes.ts
@@ -9,17 +9,19 @@ const projectRouter = t.router({
     createProject: protectedProcedure
         .input(
             z.object({
-                folder_id: z.custom<Types.ObjectId>().optional(),
-                title: z.string().min(project.title.MIN_LENGTH).max(project.title.MAX_LENGTH),
-                description: z.string().max(project.description.MAX_LENGTH).optional(),
-                body: z.string().max(project.body.MAX_LENGTH).optional(),
-                tags: z
-                    .array(z.string().max(project.tags.tag.MAX_LENGTH))
-                    .max(project.tags.MAX_LENGTH)
-                    .optional(),
-                status: z.nativeEnum(project.status).optional().default(project.status.DRAFT),
-                cover_image: z.string().optional(),
-                platforms: z.array(z.nativeEnum(user.platforms)),
+                project: z.object({
+                    folder_id: z.custom<Types.ObjectId>().optional(),
+                    title: z.string().min(project.title.MIN_LENGTH).max(project.title.MAX_LENGTH),
+                    description: z.string().max(project.description.MAX_LENGTH).optional(),
+                    body: z.string().max(project.body.MAX_LENGTH).optional(),
+                    tags: z
+                        .array(z.string().max(project.tags.tag.MAX_LENGTH))
+                        .max(project.tags.MAX_LENGTH)
+                        .optional(),
+                    status: z.nativeEnum(project.status).optional().default(project.status.DRAFT),
+                    cover_image: z.string().optional(),
+                    platforms: z.array(z.nativeEnum(user.platforms)),
+                }),
             }),
         )
         .mutation(({ input, ctx }) => new ProjectController().createProjectHandler(input, ctx)),
