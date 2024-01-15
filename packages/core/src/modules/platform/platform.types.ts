c4f1f64f5f68249cc443c30ca8c98c738ca42c6c
Rakesh Potnuru <rakeshpotnuru321@gmail.com>
2024-01-15 12:25:13 +0530

    Update package.json and platform types

--- packages/core/src/modules/platform/platform.types.ts
+++ packages/core/src/modules/platform/platform.types.ts
@@ -2,35 +2,21 @@ import type { Types } from "mongoose";
 
 import type { constants } from "../../config/constants";
 import type { IPagination } from "../../types/common.types";
-import type { IBlogger, IBloggerResponse } from "./blogger/blogger.types";
-import type { IDevTo, IDevToResponse } from "./devto/devto.types";
-import type { IGhost, IGhostResponse } from "./ghost/ghost.types";
-import type { IHashnode, IHashnodeResponse } from "./hashnode/hashnode.types";
-import type { IMedium, IMediumResponse } from "./medium/medium.types";
-import type { IWordPress, IWordPressResponse } from "./wordpress/wordpress.types";
+import type { IBloggerResponse } from "./blogger/blogger.types";
+import type { IDevToResponse } from "./devto/devto.types";
+import type { IGhostResponse } from "./ghost/ghost.types";
+import type { IHashnodeResponse } from "./hashnode/hashnode.types";
+import type { IMediumResponse } from "./medium/medium.types";
+import type { IWordPressResponse } from "./wordpress/wordpress.types";
 
 export type TPlatformName =
     (typeof constants.user.platforms)[keyof typeof constants.user.platforms];
 
-type TPlatform<T extends TPlatformName> = T extends "hashnode"
-    ? IHashnode
-    : T extends "medium"
-    ? IMedium
-    : T extends "devto"
-    ? IDevTo
-    : T extends "ghost"
-    ? IGhost
-    : T extends "wordpress"
-    ? IWordPress
-    : T extends "blogger"
-    ? IBlogger
-    : never;
-
-export interface IPlatform<T extends TPlatformName = TPlatformName> {
+export interface IPlatform {
     _id?: Types.ObjectId;
     user_id?: Types.ObjectId;
-    name: T;
-    data: TPlatform<T>;
+    name: TPlatformName;
+    data: Types.ObjectId;
     pagination?: IPagination;
 }
 
