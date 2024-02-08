06869dd97eeecfccf6204e2b0f4b438964bcd411
Rakesh Potnuru <rakeshpotnuru321@gmail.com>
2024-02-08 19:50:37 +0530

    Add Content Security Policy headers to next.config.js

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
+
+const cspHeader = `
+    default-src 'self';
+    connect-src 'self' ${process.env.NEXT_PUBLIC_CORE_URL} ${process.env.NEXT_PUBLIC_WEBSOCKET_URL} https://accounts.google.com;
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://challenges.cloudflare.com https://accounts.google.com https://media-library.cloudinary.com;
+    frame-src 'self' https://challenges.cloudflare.com https://accounts.google.com https://eml.imagekit.io https://media-library.cloudinary.com https://console.cloudinary.com;
+    style-src 'self' 'unsafe-inline' https://accounts.google.com;
+    img-src * blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    block-all-mixed-content;
+    upgrade-insecure-requests;
+`;
+
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -30,6 +47,19 @@ const nextConfig = {
     };
 
     return config;
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: cspHeader.replace(/\n/g, ""),
+          },
+        ],
+      },
+    ];
   },
 };
 
