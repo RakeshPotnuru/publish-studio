fd2e490b620e30b0e21b4c32e29740214785baa0
Rakesh Potnuru <rakeshpotnuru321@gmail.com>
2024-03-02 18:40:50 +0530

    docs: Add next-sitemap for generating sitemap.xml web, frontend: Update robots.ts

--- apps/frontend/src/app/robots.ts
+++ apps/frontend/src/app/robots.ts
@@ -7,6 +7,7 @@ export default function robots(): MetadataRoute.Robots {
       disallow: "/",
       allow: ["/login", "/register"],
     },
+    host: "https://app.publishstudio.one",
     sitemap: "https://app.publishstudio.one/sitemap.xml",
   };
 }
