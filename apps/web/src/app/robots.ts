5ac7678fca40a06b0faf8d2d7889c15338e39f1a
Rakesh Potnuru <rakeshpotnuru321@gmail.com>
2024-03-02 18:51:37 +0530

    Stg (#22)
    * Bug fixes
    
    * Add target="_blank" to Docs link in Navbar
    
    * Add utm tags for unsplash
    
    * docs: Add next-sitemap for generating sitemap.xml
    web, frontend: Update robots.ts

--- apps/web/src/app/robots.ts
+++ apps/web/src/app/robots.ts
@@ -6,6 +6,7 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
+    host: "https://publishstudio.one",
     sitemap: "https://publishstudio.one/sitemap.xml",
   };
 }
