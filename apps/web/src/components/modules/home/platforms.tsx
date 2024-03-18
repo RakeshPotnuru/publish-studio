378db01fe3dcea46c2fc9d655e88a62466fd3f84
Rakesh Potnuru <rakeshpotnuru321@gmail.com>
2024-03-18 12:16:59 +0530

    Stg (#31)
    * admin: add created to invites
    frontend: bug fixes
    
    * frontend: fix reset user state after updating platforms
    
    * Add project page image and hero section styling
    
    * fix responsiveness

--- apps/web/src/components/modules/home/platforms.tsx
+++ apps/web/src/components/modules/home/platforms.tsx
@@ -54,7 +54,16 @@ export function Platforms() {
         }}
         className="text-center"
       >
-        <div className="flex flex-col space-y-20">
+        <div className="relative z-10 -mt-96 flex justify-center">
+          <Image
+            src={Images.projectPage}
+            width={1280}
+            height={900}
+            alt="project page"
+            className="rounded-xl"
+          />
+        </div>
+        <div className="mt-24 flex flex-col space-y-20">
           <h1 className="bg-gradient-to-br from-slate-200 to-slate-400 bg-clip-text pb-4 text-5xl font-medium tracking-tight text-transparent">
             Reach your audience, <br /> wherever they are.
           </h1>
--- apps/web/src/components/ui/lamp.tsx
+++ apps/web/src/components/ui/lamp.tsx
@@ -17,13 +17,13 @@ export const LampContainer = ({
     <div
       className={cn(
         "relative flex min-h-screen flex-col items-center justify-center overflow-hidden bg-slate-950 w-full rounded-md z-0",
-        className
+        className,
       )}
     >
       <div className="z-10 w-full">
         <Navbar />
       </div>
-      <div className="relative isolate z-0 flex w-full flex-1 scale-y-125 items-center justify-center ">
+      <div className="relative isolate z-0 mt-10 flex w-full flex-1 scale-y-125 items-center justify-center sm:mt-0">
         <motion.div
           initial={{ opacity: 0.5, width: "15rem" }}
           whileInView={{ opacity: 1, width: "30rem" }}
@@ -33,12 +33,13 @@ export const LampContainer = ({
             ease: "easeInOut",
           }}
           style={{
-            backgroundImage: "conic-gradient(var(--conic-position), var(--tw-gradient-stops))",
+            backgroundImage:
+              "conic-gradient(var(--conic-position), var(--tw-gradient-stops))",
           }}
           className="bg-gradient-conic absolute inset-auto right-1/2 h-56 w-[30rem] overflow-visible from-red-500 via-transparent to-transparent text-white [--conic-position:from_70deg_at_center_top]"
         >
-          <div className="absolute  bottom-0 left-0 z-20 h-40 w-[100%] bg-slate-950 [mask-image:linear-gradient(to_top,white,transparent)]" />
-          <div className="absolute  bottom-0 left-0 z-20 h-[100%]  w-40 bg-slate-950 [mask-image:linear-gradient(to_right,white,transparent)]" />
+          <div className="absolute bottom-0 left-0 z-20 h-40 w-[100%] bg-slate-950 [mask-image:linear-gradient(to_top,white,transparent)]" />
+          <div className="absolute bottom-0 left-0 z-20 h-[100%]  w-40 bg-slate-950 [mask-image:linear-gradient(to_right,white,transparent)]" />
         </motion.div>
         <motion.div
           initial={{ opacity: 0.5, width: "15rem" }}
@@ -49,16 +50,17 @@ export const LampContainer = ({
             ease: "easeInOut",
           }}
           style={{
-            backgroundImage: "conic-gradient(var(--conic-position), var(--tw-gradient-stops))",
+            backgroundImage:
+              "conic-gradient(var(--conic-position), var(--tw-gradient-stops))",
           }}
           className="bg-gradient-conic absolute inset-auto left-1/2 h-56 w-[30rem] from-transparent via-transparent to-red-500 text-white [--conic-position:from_290deg_at_center_top]"
         >
-          <div className="absolute  bottom-0 right-0 z-20 h-[100%]  w-40 bg-slate-950 [mask-image:linear-gradient(to_left,white,transparent)]" />
-          <div className="absolute  bottom-0 right-0 z-20 h-40 w-[100%] bg-slate-950 [mask-image:linear-gradient(to_top,white,transparent)]" />
+          <div className="absolute bottom-0 right-0 z-20 h-[100%] w-40 bg-slate-950 [mask-image:linear-gradient(to_left,white,transparent)]" />
+          <div className="absolute bottom-0 right-0 z-20 h-40 w-[100%] bg-slate-950 [mask-image:linear-gradient(to_top,white,transparent)]" />
         </motion.div>
         <div className="absolute top-1/2 h-48 w-full translate-y-12 scale-x-150 bg-slate-950 blur-2xl"></div>
         <div className="absolute top-1/2 z-50 h-48 w-full bg-transparent opacity-10 backdrop-blur-md"></div>
-        <div className="absolute inset-auto z-50 h-36 w-[28rem] -translate-y-1/2 rounded-full bg-red-500 opacity-50 blur-3xl"></div>
+        <div className="absolute inset-auto z-50 h-14 w-[6rem] -translate-y-1/2 rounded-full bg-red-500 opacity-50 blur-3xl sm:h-36 sm:w-[28rem]"></div>
         <motion.div
           initial={{ width: "8rem" }}
           whileInView={{ width: "16rem" }}
@@ -83,7 +85,7 @@ export const LampContainer = ({
         <div className="absolute inset-auto z-40 h-44 w-full -translate-y-[12.5rem] bg-slate-950"></div>
       </div>
 
-      <div className="relative z-50 flex -translate-y-80 flex-col items-center px-5">
+      <div className="relative z-50 -mb-12 sm:-mb-0 flex -translate-y-80 flex-col items-center px-5">
         {children}
       </div>
     </div>
